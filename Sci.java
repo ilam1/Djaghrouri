e43b96c81d72936c0c6f4d2b6bdcecfd740d3d42
Jason Lam <jlam6@stuy.edu>
2017-01-11 11:49:48 -0500

    science updated

--- Sci.java
+++ Sci.java
@@ -1,2 +1,29 @@
 public class Sci extends Woo{
+
+    //Instance Variables
+    public String[] questions;
+    public String[] answers;
+
+    //Methods begin: 
+
+    public String forceQ() {
+    }
+    public String forceA() {
+    }
+
+    public String metricConversionsQ() {
+    }
+    public String metricConversionsQ() {
+    }
+
+    public String acidsBaseQ() {
+    }
+    public String acidsBaseA() {
+    }
+
+    public String organSystemsQ() {
+    }
+    public String organSystemsA() {
+    }
+
 }

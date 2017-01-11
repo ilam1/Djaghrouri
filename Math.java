d1cb893884269af4f3e1538de19e15a9499bb391
Bayan Berri <bberri@stuy.edu>
2017-01-11 10:46:15 -0500

    math and misc updated

--- Math.java
+++ Math.java
@@ -1,2 +1,38 @@
 public class Math extends Woo{
+    //Instance Variables    
+    public String[] questions;
+    public String[] answers;
+    //Methods begin:
+   
+    public String logQ(){
+    }
+    public int logA(){
+    }
+    
+    public String trigQ(){
+    }
+    public int trigA(){
+    }
+    
+    public String radQ(){
+    }
+    public String radA(){
+    }
+   
+    public String quadQ(){
+    }
+    public int quadA(){
+    }
+    
+    public String wordQ(){
+    }
+    public int wordA(){
+    }
+    
+    public String circleEq(){
+    }
+    public int circleEq(){
+    }
+    
+
 }
--- Misc.java
+++ Misc.java
@@ -1,2 +1,4 @@
 public class Misc extends Woo{
+ public String[] questions;
+ public String[] answers;
 }

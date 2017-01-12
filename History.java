8e145bafd5cff707f48b8f8e0451119fac42d89d
Bayan Berri <bberri@stuy.edu>
2017-01-11 21:07:20 -0500

    misc, history changed

--- History.java
+++ History.java
@@ -1,2 +1,29 @@
 public class History extends Woo{
+    //instance variables
+    public String[] questions;
+    public String[] answers;
+    //Methods begin
+    public String presidentsQ(){
+    }
+    public String presidentsA(){
+    }
+    public String geographyQ(){
+    }
+    public String geographyA(){
+    }
+    public String constitutionQ(){
+    }
+    public String constitutionA(){
+    }
+    public String warsQ(){
+    }
+    public String warsA(){
+    }
+    public String empireQ(){
+    }
+    public String empireA(){
+    }
+    //Main Method Begins.
+    public static void main(String[] args){
+    }
 }
--- Misc.java
+++ Misc.java
@@ -1,4 +1,25 @@
 public class Misc extends Woo{
- public String[] questions;
- public String[] answers;
+    // instance variables
+    public String[] questions;
+    public String[] answers;
+
+    // Methods begin
+    public String riddleQ(){
+    }
+    public String riddleA(){
+    }
+    public String punQ(){
+    }
+    public String punA(){
+    }
+    public String brainTeaserQ(){
+    }
+    public String brainTeaserA(){
+    }
+    public String logicQ(){
+    }
+    public String logicA(){
+    }
+    
+    
 }

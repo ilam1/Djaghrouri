ed02f0115737db1fd65a097759765a67c43c55d1
ilam1 <ilam1@stuy.edu>
2017-01-14 11:55:02 -0500

    Fixed syntax errors

--- China.java
+++ China.java
@@ -6,7 +6,7 @@ public class China extends Woo{
     "List the twelve Chinese zodiacs in the order by which they finished the race\nHint: Goat and Rooster are part of the list",
     "Which zodiac corresponds with the saying: Being constant without changeability leads to woodenness?",
     "The earliest evidence of Chinese script is inscribed on what object?",
-    "The slang 3Q in Chinese translates to what English phrase?",    
+    "The slang 3Q in Chinese translates to what English phrase?"};
     public static String[] answers ={"6","Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, Pig", "Rooster", "oracle bones","Thank you"};
 
 }

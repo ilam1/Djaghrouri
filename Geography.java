ec7855e07152a7cb3cee9fbdecd08bea37206cd5
ilam1 <ilam1@stuy.edu>
2017-01-14 12:37:48 -0500

    Fixed syntax errors

--- Geography.java
+++ Geography.java
@@ -6,7 +6,7 @@ public class Geography extends Woo{
     "What is the largest body of water in the world?",
     "What is the longest river in the world (Do not include river in your response)",
     "The latitudinal differences in pressure delineate a number of major pressure zones, which correspond with zones of what?",
-    "The website Taqadoumy was blocked in this country and its owner was detained for crimes against Islam after a female commenter argued for sexual freedom. Police in this country fired on protesters in Maghama who demanded equal voting rights for non-Arabs. As a result of the conflict in Western Sahara, many Sahrawis have fled to Algeria and this country. This country officially abolished slavery in 1981, but it still has the world’s highest proportion of slaves per capita. For the point, name this West African country whose capital is Nouakchott." }  
+    "The website Taqadoumy was blocked in this country and its owner was detained for crimes against Islam after a female commenter argued for sexual freedom. Police in this country fired on protesters in Maghama who demanded equal voting rights for non-Arabs. As a result of the conflict in Western Sahara, many Sahrawis have fled to Algeria and this country. This country officially abolished slavery in 1981, but it still has the world’s highest proportion of slaves per capita. For the point, name this West African country whose capital is Nouakchott." };  
     public static String[] answers ={"Atlantic","Pacific Ocean", "Amazon", "climate", "Mauritania"};
 
 }

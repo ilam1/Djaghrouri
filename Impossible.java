873fed9f776976b199a14964a71526047302aaee
ilam1 <ilam1@stuy.edu>
2017-01-14 09:19:12 -0500

    Fun facts!

---
+++ Impossible.java
@@ -0,0 +1,11 @@
+public class Impossible extends Woo{
+
+    //Instance Variables
+    public static String[] questions = {"What does the expiration date on water bottles refer to?",
+    "What is the only continent without reptiles?",
+    "Which contient hosts the largest desert in the world?",
+    "What was bubble wrap's intended use upon its creation?",
+    "What holiday is May 29th?",    
+    public static String[] answers ={"The bottle","Antarctica","Antarctica","Wallpaper","Put a Pillow on Your Fridge Day"};
+
+}

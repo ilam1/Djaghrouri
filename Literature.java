b3d6b320b51813dfbdd79c54c52e75c4b460b4ce
ilam1 <ilam1@stuy.edu>
2017-01-13 22:12:16 -0500

    Literature quotes

---
+++ Literature.java
@@ -0,0 +1,10 @@
+public class Literature extends Woo{
+
+    //Instance Variables
+    public static String[] questions = {"Name the last name of the author that wrote the below statement:\nTwo households both alike in dignity",
+    "Name the title of the book with the following statement\nIt was times like these when I thought my father, who hated guns and had never been to any wars, was the bravest man who ever lived.",
+    "Name the title of the book with the following statement\nAs he read, I fell in love the way you fall asleep: slowly, and then all at once",
+    "Name the title of the book with the following statement\nLove is the longing for the half of ourselves we have lost.",
+    "Name the full name of the author that wrote the below statement:\nA dream, all a dream, that ends in nothing, and leaves the sleeper where he lay down, but I wish you to know that you inspired it."};
+    public static String[] answers ={"Shakespeare","To Kill a Mockingbird", "The Fault in Our Stars","The Unbearable Lightness of Being","Charles Dickens"};
+}

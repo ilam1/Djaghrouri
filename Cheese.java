3b325ddd31ff999943815dd8b583e92bf1931c4f
ilam1 <ilam1@stuy.edu>
2017-01-14 14:56:47 -0500

    Cheese trivia

---
+++ Cheese.java
@@ -0,0 +1,12 @@
+public class Cheese extends Woo{
+
+    //Instance Variables
+    public static String[] questions = {
+    "What is the most eaten type of cheese?",
+    "How do you get a mouse to smile?",
+    "What do you call cheese that is sad?",
+    "What is the most expensive type of cheese?",
+    "What do you call cheese that isn't yours?"};
+    public static String[] answers ={"Cheddar","Say cheese","Blue cheese","Pule","Nacho Cheese"};
+
+}

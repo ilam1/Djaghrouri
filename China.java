2f8d7ad535e5d0dfa9cb16138ae5b549b3be77f2
ilam1 <ilam1@stuy.edu>
2017-01-14 09:38:49 -0500

    Trivia about China

---
+++ China.java
@@ -0,0 +1,12 @@
+public class China extends Woo{
+
+    //Instance Variables
+    public static String[] questions = {
+    "Approximately one in how many people speak Chinese?\n(Please state your answer as a number)",
+    "List the twelve Chinese zodiacs in the order by which they finished the race\nHint: Goat and Rooster are part of the list",
+    "Which zodiac corresponds with the saying: Being constant without changeability leads to woodenness?",
+    "The earliest evidence of Chinese script is inscribed on what object?",
+    "The slang 3Q in Chinese translates to what English phrase?",    
+    public static String[] answers ={"6","Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, Pig", "Rooster", "oracle bones","Thank you"};
+
+}

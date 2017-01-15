6507f2262c21538c384b884c72ce003fc9d3513a
jasonlam00 <jlam6@stuy.edu>
2017-01-15 18:17:32 -0500

    added questions for earth category

--- Earth.java
+++ Earth.java
@@ -1,4 +1,14 @@
 public class Earth extends Woo{
-    public static String[] questions= {};
-    public static String[] answers={};
+    public static String[] questions= {"How old is the earth?",
+                                        "How many continents are there on Earth?",
+                                        "How far are we from the sun? (in km)",
+                                        "How fast does the Earth move around the sun? (in km/s)",
+                                        "What is the force of gravity on Earth? (in m/s^2)"
+                                        };
+    public static String[] answers={"4.5 billion years old",
+                                    "7",
+                                    "150 million km",
+                                    "30 km/s",
+                                    "9.81 m/s^2"
+                                  };
 }

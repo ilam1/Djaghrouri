62154ba945e0dff787266258a9774700586f77c2
Bayan Berri <bberri@stuy.edu>
2017-01-15 14:21:31 -0500

    Women updated w Q

--- Women.java
+++ Women.java
@@ -1,4 +1,7 @@
 public class Women extends Woo{
-    public static String[] questions={};
-    public static String[] answeres={};
+    public static String[] questions={"The first women's rights convention was held here. (Don't include the state)",
+				      "In 1873 this person went on trial for voting in a Presidential election.",
+				      "This person was the first woman elected to the US senate(Last Name only)",
+				      "The Nineteenth AMendment is also known as the ___ Amendment","Who is the first woman appointed to the Supreme Court? (Full name)"};
+    public static String[] answeres={"Seneca Falls", "Susan B. Anthony","Caraway","Anthony","Sandra Day O'Connor"};
 }

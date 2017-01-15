c9794f847cec5c3dda9bb85745516759c22d1d44
Bayan Berri <Bayan Berri>
2017-01-15 11:16:23 -0500

    Astronomy questions

--- Astronomy.java
+++ Astronomy.java
@@ -1,4 +1,5 @@
 public class Astronomy extends Woo{
-    public static String[] questions={};
-    public static String[] answers={};
+    public static String[] questions={"This is a small orbiting body made up of dust,ice, and frozen gases. The solid center of this is its nucleus, and like all objects in the solar system it orbits the sun.","This is a chunk of matter that enters our atmosphere and is heated by friction with the air to the point of burning up as it falls towards the Earth","This is a large rotating cloud of dust and hydrogen gas","This is the lander craft that within the last decade brought two rovers to Mars to investigate","This planet takes 163 Earth years to orbit around the sun once"
+    };
+    public static String[] answers={"comet","Meteor","nebula","The Phoenix","Neptune"};
 }

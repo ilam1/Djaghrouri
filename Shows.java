276f2ebc10e4207ac22f95adb505ca6f69988b47
jasonlam00 <jlam6@stuy.edu>
2017-01-15 18:40:40 -0500

    added questions for shows category

--- Shows.java
+++ Shows.java
@@ -1,3 +1,12 @@
 public class Shows extends Woo{
-    public static String[] questions={};
-    public static String[] answers= {};
+    public static String[] questions={"Who was the lead actor in The Fresh Prince of Bel-Air?",
+                                      "What year did the Simpsons started to air?",
+                                      "How long were Jerry Seinfield and his pals sentenced to in the series finale?",
+                                      "What TV show does the quote, WE WERE ON BREAK!!!, come from?",
+                                      "What popular children's show debuted on PBS in 1969?"};
+    public static String[] answers= {"Will Smith",
+                                     "1989",
+                                     "1 year",
+                                     "Friends",
+                                     "Sesame Street"};
+}

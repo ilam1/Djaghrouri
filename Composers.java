ea89ebcf5b305e488d03511cb7096154c5cc3b78
Bayan Berri <bberri@stuy.edu>
2017-01-15 13:57:48 -0500

    Composers updated w Questions

--- Composers.java
+++ Composers.java
@@ -1,4 +1,4 @@
 public class Composers extends Woo{
-    public static String[] questions={};
-    public static String[] answers={};
+    public static String[] questions={"Who is the Composer of Eine Kleine Nachtmusik","What composer ended his career almost completely deaf?","Who is considered the most important Norwegian composer in the history of classical music?","What is the name of Mozart's Opera that involves a seducing nobleman who catalogues his many love affairs?"," What is the name of the ballet by Tchaikovsky that is generally performed around Christmas?"};
+    public static String[] answers={"Mozart","Beethoven","Grieg","Don Giovanni","Nutcracket Suite"};
 }

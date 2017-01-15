b4df126deaaed8b7a28601ff51c8fe1f50b707a4
jasonlam00 <jlam6@stuy.edu>
2017-01-15 18:56:26 -0500

    added questions to sport category

--- Sport.java
+++ Sport.java
@@ -1,4 +1,12 @@
 public class Sport extends Woo{
-    public static String[] questions= {};
-    public static String[] answers={};
+    public static String[] questions= {"Who is a former competitive swimmer with the most Olympic medals of all time?",
+                                       "Who won the Super Bowl in 2016?",
+                                       "In which country did MODERN day soccer originate?",
+                                       "Who is the chess champion to date?",
+                                       "Who plays the setter position in the team that won the men's volleyball championship in 2016?",};
+    public static String[] answers={"Michael Phelps",
+                                    "Denver Broncos",
+                                    "England",
+                                    "Magnus Carlsen",
+                                    "Andrew Hillman"};
 }

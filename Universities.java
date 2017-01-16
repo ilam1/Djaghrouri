ef5619a6a52c8caac201dc41b6bd6f24fe04d5a9
jasonlam00 <jlam6@stuy.edu>
2017-01-15 19:24:38 -0500

    added questions for university category

--- Universities.java
+++ Universities.java
@@ -1,4 +1,12 @@
 public class Universities extends Woo{
-    public static String[] questions={};
-    public static String[] answers={};
+    public static String[] questions={"What year was the oldest college university established in?"
+                                      "How many Ivy League college universities are there?";
+                                      "What is the name of the newest university in the U.S.?";
+                                      "What is the acceptance rate of Harvard University?";
+                                      "How many public 4-year institutions exist in the U.S.?"};
+    public static String[] answers={"1636";
+                                    "8";
+                                    "University of California Merced";
+                                    "5.4%";
+                                    "629"};
 }

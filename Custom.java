e55d3d8fedf9df544da64d866212f7b36aaf329a
Bayan Berri <bberri@stuy.edu>
2017-01-19 16:55:06 -0500

    added Custom

---
+++ Custom.java
@@ -0,0 +1,16 @@
+public class Custom extends Category{
+    public static String[] category= new String[5];
+    public static String[] questions1=new String[5];
+    public static String[] questions2=new String[5];
+    public static String[] questions3=new String[5];
+    public static String[] questions4=new String[5];
+    public static String[] questions5=new String[5];
+    public static String[] answers1=new String[5];
+    public static String[] answers2=new String[5];
+    public static String[] answers3=new String[5];
+    public static String[] answers4=new String[5];
+    public static String[] answers5=new String[5];
+
+
+
+}

aac868d43a4823c400ee5e10194ee1f97fe6b329
ilam1 <ilam1.stuy.edu>
2017-01-21 00:05:46 -0500

    Added Color! - Color scheme still in the works

--- Woo.java
+++ Woo.java
@@ -31,6 +31,18 @@ public class Woo {
      "Pop", "Women", "USA", "College", "Plays",
      "Food", "Tech", "Shows", "Music", "Earth", "Culture",
      "Literature", "Space", "Sports"};
+
+    //COLORS
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+        
     String[] newCat = createCategory();
     String buzzed = "";
 
@@ -198,7 +210,7 @@ public class Woo {
 	    
 	}
 
-	System.out.println("\nHow many players are playing in total?"); //Currently only supports a single player
+	System.out.println(ANSI_PURPLE + "\nHow many players are playing in total?" + ANSI_RESET); 
         try {
             totalPlayers = Integer.parseInt(in.readLine());
         } catch (IOException e) {

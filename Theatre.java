8c3cdf95fbda4cb6634f353bc809d95549e353cd
Bayan Berri <bberri@stuy.edu>
2017-01-22 12:38:22 -0500

    fixed theatre

--- Theatre.java
+++ Theatre.java
@@ -1,4 +1,4 @@
 public class Theatre extends Category{
-    public static String[] questions={"This musical tells the story about the Witches of Oz.","This musical tells the story of a young man that inherits his father's shoe factory and his life takes an unlikely turn","This play by Christopher Durang is about a terrible blind date","What is Minna's brother's name in the play 'Minna and the Space People'","", "Which broadway show is the longest running show in broadway history?"};
+    public static String[] questions={"This musical tells the story about the Witches of Oz.","This musical tells the story of a young man that inherits his father's shoe factory and his life takes an unlikely turn","This play by Christopher Durang is about a terrible blind date","What is Minna's brother's name in the play 'Minna and the Space People'", "Which broadway show is the longest running show in broadway history?"};
     public static String[] answers={"Wicked","Kinky boots","Beyond Therapy","Brett","The Phantom of the Opera"};
 }

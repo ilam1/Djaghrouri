4eb5554676c1b059d33c8f672a096daca808114f
ilam1 <ilam1@stuy.edu>
2017-01-21 20:14:19 -0500

    Update Stuy.java

--- Stuy.java
+++ Stuy.java
@@ -4,6 +4,6 @@ public class Stuy extends Category{
     public static String[] questions = {"What train is the greatest cause of stress?\n(Please answer with a letter or number)",
     "What historical figure has a landing in front of the Tribeca Bridge?","How many steps does the Tribeca Bridge have?",
     "The Pegleg is an epithet of who?","Who is this Stuy CS Final Project dedicated to?"};
-    public static String[] answers ={"7","Frederick Douglass","to be determined","Peter Stuyvesant","Ms. Djaghrouri"};
+    public static String[] answers ={"7","Frederick Douglass","35","Peter Stuyvesant","Ms. Djaghrouri"};
     
 } // end class Stuy
